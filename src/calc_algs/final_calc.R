14cddafa050f00fdb2331668452db26c3fa4dc5b
Aderson <adersonrs@gmail.com>
2025-06-18 13:42:12 -0300

    feat: atualizado

--- src/calc_algs/final_calc.R
+++ src/calc_algs/final_calc.R
@@ -20,8 +20,8 @@ all_returns_weighted_info <- daily_B_H_return_weighted_info$annual_returns %>%
 
 diferenca_media_anual <- tibble(
   Ano = all_returns_weighted$Ano,
-  `fama&french_info_retorno` = all_returns_weighted_info$media_retorno - all_returns_weighted$media_retorno,
-  `fama&french_info_sharpe` = all_returns_weighted_info$media_sharpe_ratio - all_returns_weighted$media_sharpe_ratio,
+  `info_retorno` = (all_returns_weighted_info$media_retorno + all_returns_yearly_bal_weighted_info$media_retorno) - (all_returns_weighted$media_retorno),
+  `info_sharpe` = all_returns_weighted_info$media_sharpe_ratio - all_returns_weighted$media_sharpe_ratio,
   
   `fama&french_info_bal_retorno` = all_returns_yearly_bal_weighted_info$media_retorno - all_returns_yearly_bal_weighted$media_retorno,
   `fama&french_info_bal_sharpe` = all_returns_yearly_bal_weighted_info$media_sharpe_ratio - all_returns_yearly_bal_weighted$media_sharpe_ratio,
@@ -120,7 +120,7 @@ w_groups_all_returns_markovitz <- daily_w_groups_return_markovitz_info$annual_re
     media_retorno = mean(retorno_excedente),
     media_sharpe_ratio = mean(sharp_ratio)
   )
-
+  
 all_not_w_groups_markovitz <- all_returns_markovitz %>%
   full_join(all_returns_markovitz_info) %>%
   full_join(all_returns_markovitz_yearly_bal) %>%
@@ -140,16 +140,16 @@ all_returns_markovitz_difference <- tibble(
   info_bal_sharpe = daily_w_groups_return_markovitz_yearly_bal_info$annual_returns$sharp_ratio - daily_w_groups_return_markovitz_yearly_bal$annual_returns$sharp_ratio,
   
   balanced = (daily_w_groups_return_markovitz_yearly_bal_info$annual_returns$retorno_excedente + daily_w_groups_return_markovitz_yearly_bal$annual_returns$retorno_excedente) 
-  - (daily_w_groups_return_markovitz_info$annual_returns$retorno_excedente + daily_w_groups_return_markovitz$annual_returns$retorno_excedente),
+    - (daily_w_groups_return_markovitz_info$annual_returns$retorno_excedente + daily_w_groups_return_markovitz$annual_returns$retorno_excedente),
   
   balanced_sharpe = (daily_w_groups_return_markovitz_yearly_bal_info$annual_returns$sharp_ratio + daily_w_groups_return_markovitz_yearly_bal$annual_returns$sharp_ratio) 
   - (daily_w_groups_return_markovitz_info$annual_returns$sharp_ratio + daily_w_groups_return_markovitz$annual_returns$sharp_ratio),
-  
-  
+    
+
   w_groups_vs_groups_returns = w_groups_all_returns_markovitz$media_retorno - all_not_w_groups_markovitz$media_retorno,
   w_groups_vs_groups_sharpe = w_groups_all_returns_markovitz$media_sharpe_ratio - all_not_w_groups_markovitz$media_sharpe_ratio
 )
-
+  
 
 #----------------------------------------------------  PONDERADO PELO VALOR DE MERCADO ---------------------------------------------------------------------------------------
   
@@ -267,4 +267,29 @@ all_returns_markovitz_difference <- tibble(
   daily_w_groups_return_markovitz_yearly_bal_info$annual_returns <- daily_w_groups_return_markovitz_yearly_bal_info$annual_returns %>% arrange(Ano) %>% mutate(retorno_acumulado = cumprod(1+return)-1, retorno_ativo_livre_risco_acumulado = cumprod(1+retorno_IBOV)-1, retorno = return)
     
     
+  
+  
+  # Carregar a biblioteca necessária
+  library(openxlsx)
+  
+  # Método 1: Criar duas arquivos Excel separados
+  write.xlsx(diferenca_media_anual, "diferenca_media_anual.xlsx", rowNames = TRUE)
+  write.xlsx(all_returns_markovitz_difference, "all_returns_markovitz_difference.xlsx", rowNames = TRUE)
+  
+  # Método 2: Criar um único arquivo Excel com duas abas
+  wb <- createWorkbook()
+  
+  # Adicionar primeira aba
+  addWorksheet(wb, "Diferenca_Media_Anual")
+  writeData(wb, "Diferenca_Media_Anual", diferenca_media_anual, rowNames = TRUE)
+  
+  # Adicionar segunda aba
+  addWorksheet(wb, "Returns_Markovitz_Diff")
+  writeData(wb, "Returns_Markovitz_Diff", all_returns_markovitz_difference, rowNames = TRUE)
+  
+  # Salvar o arquivo
+  saveWorkbook(wb, "analise_financeira.xlsx", overwrite = TRUE)
+  
+  print("Arquivos Excel criados com sucesso!")
+  
  
